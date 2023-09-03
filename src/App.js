67be382fdca04ec51e121a985301b1d345524558
Kike-Elvira <joseelvirarivas01@gmail.com>
2023-09-03 15:47:24 -0600

    Curso(L.2)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
+import Header from './components/Header';
 import Home from './pages/Home';
 import Page404 from './pages/Page.404';
 import Sobre from './pages/Sobre';
@@ -8,13 +9,14 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 function App() {
   return (
    
-        <Router>
-          <Routes>
-            <Route path='/' element={<Home></Home>}></Route>
-            <Route path='/sobre' element={<Sobre></Sobre>}></Route>
-            <Route path='*' element={<Page404></Page404>}></Route>
-          </Routes>
-        </Router>
+    <Router>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/sobre' element={<Sobre></Sobre>}></Route>
+        <Route path='*' element={<Page404></Page404>}></Route>
+      </Routes>
+    </Router>
         
    
   );
